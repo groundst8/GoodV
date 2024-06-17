b40f91f3f279ad3d2a06713c8c99629a1c0b05d5
Nick <53413353+nickpalladino@users.noreply.github.com>
2024-06-17 17:47:47 -0400

    Added required PendingIntent flag for Android 12+

--- app/src/main/java/com/kk4vcz/goodv/MainActivity.java
+++ app/src/main/java/com/kk4vcz/goodv/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity
 
 
         PendingIntent intent = PendingIntent.getActivity(this, 0,
-                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_IMMUTABLE);
         NfcAdapter.getDefaultAdapter(this).enableForegroundDispatch(this, intent, null, null);
     }
 
